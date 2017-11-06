963619438ecacd2db1d1894b0614efb2bb60112c
HAM2506 <513279703@QQ.COM>
2017-11-06 18:30:38 +0800

    update

--- django/first_test/front_api/src/components/HelloFromVux.vue
+++ django/first_test/front_api/src/components/HelloFromVux.vue
@@ -1,21 +1,38 @@
 <template>
 	<div>
-		<group title="cell demo">
+		<x-header :left-options="{showBack: false}">微信
+			<span slot="right">
+				<i class="iconfont icon-more" @click="showMore"></i>
+			</span>
+		</x-header>
+		<group title="微信">
 			<cell title="abc" value="abc" is-link></cell>
+			<x-input title="用户名" v-model="value"></x-input>
 		</group>
+		<div class="more-menu">
+
+		</div>
 	</div>
 </template>
 
 <script>
-	import { Group, Cell } from 'vux'
+	import { Group, Cell, XInput, XHeader } from 'vux'
 	export default {
 		components: {
 			Group,
-			Cell
+			Cell,
+			XInput,
+			XHeader
 		},
 		data () {
 			return {
-				msg: 'Hello World!'
+				msg: 'Hello World!',
+				value: 123
+			}
+		},
+		methods: {
+			showMore() {
+				console.info(0)
 			}
 		},
 		mounted() {
@@ -28,12 +45,35 @@
 	}
 </script>
 
-<style>
+<style scoped>
 	.vux-demo {
 		text-align: center;
 	}
 	.logo {
 		width: 100px;
 		height: 100px
+	}
+	.iconfont {    
+		font-size: 20px;
+    	font-weight: bold;
+	}
+	.more-menu {
+		position: absolute;
+		top: 60px;
+		right: 8px;
+		width: 120px;
+		height: 140px;
+		background: #35495e;
+	}
+	.more-menu:before {
+		content: '';
+		position: absolute;
+		top: -8px;
+    	right: 14px;
+		width: 0;
+		height: 0;
+		border-left: 8px solid transparent;
+		border-right: 8px solid transparent;
+		border-bottom: 10px solid #35495e;
 	}
 </style>
